e942ba0c512bf2188ec5cfc167eee52dfa6a7890
Maxim S. Ivanov <ivanenok@gmail.com>
2015-11-27 23:24:24 +0600

    domain structure on server created

---
+++ server/src/main/java/dukemarket/Application.java
@@ -0,0 +1,12 @@
+package dukemarket;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * This file created by Maxim S. Ivanov
+ */
+@SpringBootApplication
+@EnableJpaRepositories
+public class Application {
+}
