5114c541572a539c2bc385905fb3d40b14c0b664
Maxim S. Ivanov <ivanenok@gmail.com>
2015-11-29 15:38:34 +0600

    parameters option disabled

--- server/src/main/java/dukemarket/repositories/ApplicationRepository.java
+++ server/src/main/java/dukemarket/repositories/ApplicationRepository.java
@@ -3,6 +3,7 @@ package dukemarket.repositories;
 import dukemarket.domain.DukeApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,5 +17,5 @@ public interface ApplicationRepository extends JpaRepository<DukeApplication, Lo
     DukeApplication findByKey(String key);
 
     @Query("select app from DukeApplication app where app.customer.key = :customer")
-    List<DukeApplication> findByCustomer(String customer);
+    List<DukeApplication> findByCustomer(@Param("customer") String customer);
 }
