7563259ebc3da7f246f2953ed160e5919627d93c
Maxim S. Ivanov <ivanenok@gmail.com>
2015-11-29 12:50:47 +0600

    no message

--- server/src/main/java/dukemarket/repositories/ApplicationRepository.java
+++ server/src/main/java/dukemarket/repositories/ApplicationRepository.java
@@ -2,7 +2,6 @@ package dukemarket.repositories;
 
 import dukemarket.domain.DukeApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,7 +12,6 @@ import java.util.List;
 public interface ApplicationRepository extends JpaRepository<DukeApplication, Long> {
     DukeApplication findByKey(String key);
 
-    @Query("select apps from DukeApplication app where app.customer.key = :customer")
+//    @Query("select apps from DukeApplication app where app.customer.key = :customer")
     List<DukeApplication> findByCustomer(String customer);
-
 }
